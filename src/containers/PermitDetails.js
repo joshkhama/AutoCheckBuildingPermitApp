e0c8914772856e5fb24dfe3953bbba97d1f27ea3
Khama Joshua <kowhegbajosh@gmail.com>
2021-09-22 00:14:03 +0100

    updated permit details

--- src/containers/PermitDetails.js
+++ src/containers/PermitDetails.js
@@ -4,8 +4,24 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { selectedPermit } from "../redux/actions/permitActions";
 
+import './index.css';
+
 const PermitDetails = () => {
     const permit = useSelector((state) => state.permit);
+    const {
+        id,
+        permit_,
+        permit_type,
+        review_type,
+        application_start_date,
+        issue_date,
+        processing_time,
+        street_number,
+        street_name,
+        total_fee,
+        reported_cost,
+        work_description,
+    } = permit;
     const { permitId } = useParams();
     const dispatch = useDispatch();
 
@@ -17,7 +33,8 @@ const PermitDetails = () => {
             .catch((err) => {
             console.log("Err", err);
         });
-        dispatch(selectedPermit(response.data));
+        console.log(response.data[0]);
+        dispatch(selectedPermit(response.data[0]));
     };
 
     useEffect(() => {
@@ -25,14 +42,45 @@ const PermitDetails = () => {
     }, [permitId]);
 
     return (
-        <div>
-            <div>
-                <p>
-                    <span>id: {}</span> &emsp; <span>permit type: {}</span> &emsp; <span>street name: {}</span>
-                </p>
+        
+            <div className="ui placeholder segment">
+                <div className="ui two column stackable center aligned">
+                    <p>
+                        <span>id: </span> &emsp; {id}<br />
+                    </p>
+                    <p>
+                        <span>Permit: </span> &emsp; {permit_}<br />
+                    </p>
+                    <p>
+                        <span>Permit Type: </span> &emsp; {permit_type}<br />
+                    </p>
+                    <p>
+                        <span>Review Type: </span> &emsp; {review_type}<br />
+                    </p>
+                    <p>
+                        <span>Application start date: </span> &emsp; {application_start_date}<br />
+                    </p>
+                    <p>
+                        <span>Date Issued: </span> &emsp; {issue_date}<br />
+                    </p>
+                    <p>
+                        <span>Processing Time: </span> &emsp; {processing_time}<br />
+                    </p>
+                    <p>
+                        <span>Address: </span> &emsp; {street_number}&emsp;{street_name}<br />
+                    </p>
+                    <p>
+                        <span>Total Fees paid: </span> &emsp; ${total_fee}<br />
+                    </p>
+                    <p>
+                        <span>Project Cost: </span> &emsp; {reported_cost}<br />
+                    </p>
+                    <p>
+                        <span>Work Description: </span> &emsp; {work_description}<br />
+                    </p>
+                </div>
             </div>
-        </div>
-    )
+    );
 }
 
 export default PermitDetails;
